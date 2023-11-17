98b8615901c9da2a5472db6ee33e7e755f353e7b
aditya98899 <adiverma9889@gmail.com>
2023-11-17 18:21:33 +0530

    todo list part 1

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name="home"),
+    path('todo/add', views.add_task, name="add"),
+    path('todo/update/<int:id>', views.update_task, name="update"),
 ]
