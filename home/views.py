09a8dc9502a79717f7d9c53015e23739adeb2f7c
aditya98899 <adiverma9889@gmail.com>
2023-11-23 18:33:55 +0530

    Add_upate_delete_tasks

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Task
+from django_htmx.http import HttpResponseClientRefresh
 
 # Create your views here.
 def index( request):
@@ -9,17 +10,23 @@ def index( request):
     return render(request, 'index.html',  context)
 
 def add_task(request):
-    if request.method == 'post' :
+    if request.method == 'POST' :
         task = request.POST.get('task')
         if len(task) >=2:
             Task.objects.create(title=task)
-    redirect('home')
+        print("task",task)
+    return HttpResponseClientRefresh()
     
 
 def update_task(request, id):
-    context = {}
-    return render(request, 'todo_list.html',  context)
-def delete_task(request, id)
-    pass
+    print('checkbox clicked')
+    print('the id is',id)
+    Task.objects.filter(id=id).update(is_competed=True)
+    return HttpResponseClientRefresh()
+
+def delete_task(request, id):
+    Task.objects.filter(id=id).delete()
+    return HttpResponseClientRefresh()
+    
 
 
